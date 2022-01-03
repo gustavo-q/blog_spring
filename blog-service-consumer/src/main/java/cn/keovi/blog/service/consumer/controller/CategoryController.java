4126fea7b7bfcc81f8ea2688623cb119fe1e8e41
gustavo-q <l1574802397@gmail.com>
2022-01-03 13:22:46 +0800

    分类功能

--- blog-service-consumer/src/main/java/cn/keovi/blog/service/consumer/controller/CategoryController.java
+++ blog-service-consumer/src/main/java/cn/keovi/blog/service/consumer/controller/CategoryController.java
@@ -1,10 +1,12 @@
 package cn.keovi.blog.service.consumer.controller;
 
-import cn.keovi.blog.service.consumer.service.TapService;
+import cn.keovi.blog.service.consumer.service.ArticleCategoryService;
 import cn.keovi.blog.service.consumer.session.LoginManager;
 import cn.keovi.constants.Result;
-import cn.keovi.crm.po.Article;
+import cn.keovi.crm.dto.BaseDto;
+import cn.keovi.crm.dto.CommentDto;
 import cn.keovi.crm.po.ArticleCategory;
+import cn.keovi.crm.po.Comment;
 import cn.keovi.crm.po.Tap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,61 +16,61 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * @ClassName TapController
- * @Description 标签
+ * @ClassName CategoryController
+ * @Description 分类
  * @Author gustavo
- * @Date 2022/01/02/21:54
+ * @Date 2022/01/03/11:53
  */
+
+
 @Slf4j
 @RestController
-@RequestMapping("/tap")
-public class TapController {
+@RequestMapping("/category")
+public class CategoryController {
+
 
     @Autowired
     private LoginManager loginManager;
 
     @Autowired
-    private TapService tapService;
+    private ArticleCategoryService articleCategoryService;
+
 
     //新增修改标签
-    @PostMapping("/addTap")
-    public Result addTap(@RequestBody Tap tap){
+    @PostMapping("/addCategory")
+    public Result addCategory(@RequestBody ArticleCategory articleCategory){
         try {
             if (loginManager.getUserId()==null) return Result.error("登录失效！");
-            if (tap.getId()!=null){
-                tap.setLastUpdateBy(loginManager.getUserId());
-                tap.setLastUpdateTime(new Date());
-            }else {
-                tap.setCreateBy(loginManager.getUserId());
-                tap.setCreateTime(new Date());
-            }
-            if (tapService.saveOrUpdate(tap)){
+            articleCategory.setCreateBy(loginManager.getUserId());
+            articleCategory.setCreateTime(new Date());
+            if (articleCategoryService.saveOrUpdate(articleCategory)){
                 return Result.ok();
             }
             return Result.error();
         } catch (Exception e) {
-            log.error("文章显示错误!", e);
+            log.error("分类显示错误!", e);
             return Result.error(500, e.getMessage());
 
         }
     }
 
-    //删除标签
-    @DeleteMapping("/deleteTap/{id}")
-    public Result deleteTap(@PathVariable Long id){
+    //删除分类
+    @DeleteMapping("/deleteCategory/{id}")
+    public Result deleteCategory(@PathVariable Long id){
         try {
             if (loginManager.getUserId()==null) return Result.error("登录失效！");
-            if (tapService.lambdaUpdate().set(Tap::getIsDelete, 1).set(Tap::getLastUpdateTime,new Date())
-                    .set(Tap::getLastUpdateBy,loginManager.getUserId()).eq(Tap::getId, id).update()) {
+            if (articleCategoryService.lambdaUpdate().set(ArticleCategory::getIsDelete, 1).set(ArticleCategory::getLastUpdateTime,new Date())
+                    .set(ArticleCategory::getLastUpdateBy,loginManager.getUserId()).eq(ArticleCategory::getId, id).update()) {
                 log.info("删除成功,id{}", id);
                 return Result.ok("删除成功！");
             }
             return Result.error("删除失败！");
         }catch (Exception e){
-            log.error("文章删除失败!", e);
+            log.error("分类删除失败!", e);
             return Result.error(500, e.getMessage());
         }
     }
+
 
 
     //根据用户id查询标签
@@ -76,9 +78,9 @@ public class TapController {
     public Result pageList() {
         try {
             if (loginManager.getUserId()==null) return Result.error("登录失效！");
-            List<Tap> tapList=tapService.lambdaQuery().eq(Tap::getCreateBy,loginManager.getUserId())
-                    .eq(Tap::getIsDelete,0).list();
-            return Result.ok().data(tapList);
+            List<ArticleCategory> articleCategories=articleCategoryService.lambdaQuery().eq(ArticleCategory::getCreateBy,loginManager.getUserId())
+                    .eq(ArticleCategory::getIsDelete,0).list();
+            return Result.ok().data(articleCategories);
         } catch (Exception e) {
             log.error("文章显示错误!", e);
             return Result.error(500, e.getMessage());
@@ -86,7 +88,5 @@ public class TapController {
         }
 
     }
-
-
 
 }
