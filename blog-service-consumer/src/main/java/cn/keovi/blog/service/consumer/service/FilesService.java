1fde61f2706283a473d7b2fd60e3f36b791ffcfe
gustavo-q <l1574802397@gmail.com>
2022-01-31 01:01:00 +0800

    文件上传minio

--- blog-service-consumer/src/main/java/cn/keovi/blog/service/consumer/service/FilesService.java
+++ blog-service-consumer/src/main/java/cn/keovi/blog/service/consumer/service/FilesService.java
@@ -16,5 +16,5 @@ import java.util.List;
 public interface FilesService extends IService<Files>{
 
 
-        String saveFiles(MultipartFile files) throws IOException;
+        String saveFiles(MultipartFile files) throws IOException, Exception;
     }
--- blog-service-consumer/src/main/java/cn/keovi/blog/service/consumer/serviceImpl/FilesServiceImpl.java
+++ blog-service-consumer/src/main/java/cn/keovi/blog/service/consumer/serviceImpl/FilesServiceImpl.java
@@ -1,7 +1,9 @@
 package cn.keovi.blog.service.consumer.serviceImpl;
 
+
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.IdUtil;
+import io.minio.MinioClient;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import cn.keovi.crm.po.Files;
@@ -9,7 +11,10 @@ import cn.keovi.blog.service.consumer.mapper.FilesMapper;
 import cn.keovi.blog.service.consumer.service.FilesService;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import java.io.FileInputStream;
+
+import static cn.keovi.constants.Minio.*;
+
 
 /**
  * @ClassName FilesServiceImpl
@@ -18,15 +23,27 @@ import java.io.IOException;
  * @Date 2021/12/23/21:07
  */
 @Service
-public class FilesServiceImpl extends ServiceImpl<FilesMapper, Files> implements FilesService{
+public class FilesServiceImpl extends ServiceImpl<FilesMapper, Files> implements FilesService {
 
     @Override
-    public String saveFiles(MultipartFile file) throws IOException {
-        String originalFilename = file.getOriginalFilename();  // 获取源文件的名称
-        // 定义文件的唯一标识（前缀）
-        String flag = IdUtil.fastSimpleUUID();
-        String rootFilePath = System.getProperty("user.dir") + "/files/" + flag + "_" + originalFilename;  // 获取上传的路径
-        FileUtil.writeBytes(file.getBytes(), rootFilePath);  // 把文件写入到上传的路径
-        return "files/" + flag + "_" + originalFilename;  // 返回结果 url
+    public String saveFiles(MultipartFile file){
+        String url2=null;
+            try {
+                String bucket = "public";
+                // 参数为：图床，账号，密码
+                // 创建桶
+                makeBucket(bucket);
+                setBucketPolicy(bucket, "read-write");
+                //参数为：文件夹，要存成的名字，要存的文件
+                String originalFilename = file.getOriginalFilename();  // 获取源文件的名称
+                // 定义文件的唯一标识（前缀）
+                String flag = IdUtil.fastSimpleUUID();
+                String rootFilePath =flag + "_" + originalFilename;  // 获取上传的路径
+                 url2 = uploadInputStream(bucket, rootFilePath, file.getInputStream());
+                System.out.println(url2);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return url2;
     }
 }
